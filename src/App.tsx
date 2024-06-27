56054b4c9d543d29edfa06908010d0dc5da96a91
Yxgg <52398622@qq.com>
2024-06-27 11:07:19 +0800

    Create

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuManager from './components/Menu.tsx';
+
+const App: React.FC = () => (
+    <div>
+            <MenuManager/>
+
+    </div>
+
+);
+
+export default App;
